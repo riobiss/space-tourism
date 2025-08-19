1c00e863e100990c60ed7cbd769ceed62d1f5753
Riobiss <riobisofc@gmail.com>
2025-08-19 14:03:19 -0300

    "Peladinho, começo de tudo"

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import react from "react";
+
+function App() {
+  return (
+  <div>
+    <nav>
+      <ul>
+        <li>00 Home</li>
+        <li>01 Destination</li>
+        <li>02 Crew</li>
+        <li>03 Technology</li>
+      </ul>
+    </nav>
+    So, you want to travel to Space Let’s face it; if you want to go to space,
+    you might as well genuinely go to outer space and not hover kind of on the
+    edge of it. Well sit back, and relax because we’ll give you a truly out of
+    this world experience! Explore
+  </div>)
+}
+
+export default App;
